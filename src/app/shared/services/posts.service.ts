c44010c1600dfb35712a4422be095f0b4db1fb20
Ildiko Toth <ildiko.toth@sainsburysebooks.co.uk>
2017-06-20 15:09:58 +0100

    Adding DI tio the postsservice

--- src/app/shared/services/posts.service.ts
+++ src/app/shared/services/posts.service.ts
@@ -3,13 +3,14 @@ import { Http } from "@angular/http";
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import {Posts} from '../models/Posts';
 
 @Injectable()
 export class PostsService {
 
   constructor(private http: Http) { }
 
-  public get(): Observable<Array<any>> {
+  public get(): Observable<Array<Posts>> {
     return this.http.get('/api/posts')
       .map((response) => response.json());
   }
